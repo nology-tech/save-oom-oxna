4504eff1f3e977f244e340c50c833ad46796ea7d
Dan Forder <danfrdr@gmail.com>
2022-06-16 15:16:22 +0100

    update timer test

--- src/components/Timer/Timer.test.jsx
+++ src/components/Timer/Timer.test.jsx
@@ -2,6 +2,9 @@ import { screen } from "@testing-library/react";
 import { customRender } from "../../utils/testUtils";
 import Timer from "./Timer";
 
+jest.useFakeTimers();
+jest.spyOn(global, "setTimeout");
+
 it("Should render the timer", () => {
   customRender(<Timer startTime={60} />);
 
